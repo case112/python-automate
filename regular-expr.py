c31dcd3f50b645f7217ca2bbbaa809fa9a20a4a2
Oskar <case112@users.noreply.github.com>
2020-05-08 21:17:26 +0300

    Update regular-expr.py

--- regular-expr.py
+++ regular-expr.py
@@ -1,5 +1,5 @@
 import re
 
-phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phone_num_regex = re.compile(r'(\d\d\d)?-\d\d\d-\d\d\d\d')
 
-print(phone_num_regex.findall('Reguasd fsdim 345-345-1243 fdg  34345 234-234-2234 23424324342342334'))
+print(phone_num_regex.findall('Reguasd fsdim 345-1243 fdg  34345 234-234-2234 23424324342342334'))
