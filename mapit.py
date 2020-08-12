b13fefaee18db836fd1dbe48f9eff29d44453d23
Oskar <case112@users.noreply.github.com>
2020-08-12 17:50:29 +0300

    Mapit, opens maps on location copied to your clipboard

---
+++ mapit.py
@@ -0,0 +1,10 @@
+import sys, webbrowser, pyperclip
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+webbrowser.open(
+    'https://google.com/maps/place/' + address)
+
---
+++ open-read.py
@@ -0,0 +1,6 @@
+import os, sys
+import webbrowser
+
+#open and read file from exec dir
+#f = open(os.path.join(sys.path[0], "file.txt"), "r")
+#print(f.read())
