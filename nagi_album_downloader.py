4e3ac7102396e0299a74c4032b997b1f759f54e8
Oskar <case112@users.noreply.github.com>
2020-09-04 16:20:25 +0300

    Hardcoded url download works

--- nagi_album_downloader.py
+++ nagi_album_downloader.py
@@ -2,19 +2,31 @@ import bs4
 import requests
 import os
 import shutil
+import sys
 
 #print('Album first image link here:')
-url = 'http://fotoalbum.ee/photos/Meriliiin/98994725/'
+url = 'http://fotoalbum.ee/photos/Meriliiin/101783435'
 token = True
 img_no = 0
 
 #get file name
-res = requests.get(url)
-res.raise_for_status()
+try:
+    res = requests.get(url)
+    res.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    raise SystemExit(err)
+
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
-file_name = soup.select('.breadcrumbs > a:nth-child(2)')
-file_name = file_name[0].text.strip().lower().replace(".", "").replace(",", "").replace(" ", "") 
-print(file_name)
+album_name_soup = soup.select('.breadcrumbs > a:nth-child(2)')
+
+if album_name_soup == []:
+    sys.exit("This is not a valid URL. Try again.")
+
+album_name = album_name_soup[0].text.strip().lower().replace(".", "").replace(",", "").replace(" ", "") 
+print('Downloading from album: ' + album_name)
+
+if not os.path.exists(os.getcwd() + album_name):
+    os.makedirs(album_name, exist_ok=True)
 
 while token == True:
     res = requests.get(url)
@@ -26,7 +38,7 @@ while token == True:
     for link in image_element:
         image_url = link.get('src')[2:]
         image_url = 'http://' + image_url
-        print(image_url)
+        #print(image_url)
 
 
     img_request = requests.get(image_url, stream=True)
@@ -35,26 +47,23 @@ while token == True:
         img_no += 1
         img_no = str(img_no)
 
-        if not os.getcwd()(file_name):
-            os.makedirs(file_name)
-        img_name = os.getcwd() + file_name + '_img_' + img_no + '.jpg'
+        img_path = os.getcwd() + os.path.sep + album_name + os.path.sep + album_name + '_img_' + img_no + '.jpg'
+        image_name = album_name + '_img_' + img_no + '.jpg'
+        print("Get - " + image_name)
 
-        with open(img_name, 'wb') as f:
+        with open(img_path, 'wb') as f:
             img_request.raw.decode_content = True
             shutil.copyfileobj(img_request.raw, f) 
- 
 
-        #with open(img_name, 'wb') as f:
-            #f.write(img_request.content)
 
     next_element = soup.select('.next')
     if next_element == []:
-        print('Reached to the end of the album')
+        print('Reached to the end of the album, downloaded ' + img_no + 'images.')
         token = False
     else:
         for link in next_element:
             next_url = link.get('href')
             next_url = 'http://fotoalbum.ee' + next_url
-            print(next_url)
+            #print(next_url)
             url = next_url
 
